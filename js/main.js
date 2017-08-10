1f9e06946ef235a351d274b0ec5ca5b021564dc6
Alex Rodgers <alex.e.rodgers@gmail.com>
2017-08-09 20:38:16 -0700

    I added a directions html page as well as editing the creators page more but I have not fixed a but where the instagram logo will randomly become 10x larger than it should be.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-(function() {
-"use strict";
-}) ();
+var gMapAPIKey = 'AIzaSyAZnGs4TVezTEsp_mOLgsCjWv2sNSwpCdQ';
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('gMap'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+}
