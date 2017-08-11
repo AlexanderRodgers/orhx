53b1b0c1207f207c97c1804b80aa7c831a383fbe
Alex Rodgers <alex.e.rodgers@gmail.com>
2017-08-10 19:28:23 -0700

    Added a google maps api to the directions page and it actually works so there, mom.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
-var gMapAPIKey = 'AIzaSyAZnGs4TVezTEsp_mOLgsCjWv2sNSwpCdQ';
-
 var map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('gMap'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
+    var myLoc = {lat: 38.6450919, lng: -121.161911}; 
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: myLoc,
+      zoom: 14
     });
-}
+    
+    var marker = new google.maps.Marker({
+        'position': myLoc,
+        'map': map,
+        'title': 'Intel Corporation'
+    });
+    
+    var contentString = 'Intel Corporation<br>';
+        contentString += '1900 Prarie City Rd<br>';
+        contentString += 'Folsom, CA 95630';
+    
+    var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+    
+    marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
+  }
