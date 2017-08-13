4d1a0680767e9f3123af9c0be9ef303bf4094051
Alex Rodgers <alex.e.rodgers@gmail.com>
2017-08-13 01:28:21 -0700

    made improvements

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ function initMap() {
     var contentString = 'Intel Corporation<br>';
         contentString += '1900 Prarie City Rd<br>';
         contentString += 'Folsom, CA 95630';
+        contentString += '<br><a href="https://www.google.com/maps?ll=38.646941,-121.158585&z=16&t=m&hl=en-US&gl=US&mapclient=apiv3&cid=13339657335930848820">View on Google Maps</a>'
     
     var infowindow = new google.maps.InfoWindow({
           content: contentString
