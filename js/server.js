1fc64867dd99bac392a79e6b1fba02f766289257
Alex <alex.e.rodgers@gmail.com>
2017-11-16 16:52:52 -0800

    Removed some non-necessary files

--- js/server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello world');
-    response.end();
-}
-
-http.createServer().listen(8000);
